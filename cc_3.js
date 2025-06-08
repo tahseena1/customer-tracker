ac619544ea3f26f5082324ef62b7d82964ee4aa5
Tahseena Badat <tahseena@usf.edu>
2025-06-09 01:43:41 +0200

    Created Customer Records

---
+++ cc_3.js
@@ -0,0 +1,14 @@
+let customers = [
+    {Name: "Jennie Johnson",
+     email: "jjohnson@email.com",
+     purchases: ["Small Package", "XL Bundle"]
+    }, 
+    {Name: "Kathy Lewis",
+    email: "kathyl123@email.com",
+    purchases: ["Medium Package", "Large Bundle", "XL Bundle"]
+    }, 
+    {Name: "Lilly Blue",
+        email: "lillybillyblue@email.com", 
+        purchases: ["Small Package", "Medium Package", "XL Bundle"]
+    }
+];
