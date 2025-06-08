7614d3a497d9e182a2a735a387973647e11b9194
Tahseena Badat <tahseena@usf.edu>
2025-06-09 01:49:57 +0200

    Added and Removed Customers

--- cc_3.js
+++ cc_3.js
@@ -1,14 +1,23 @@
-let customers = [
+const customers = [
     {Name: "Jennie Johnson",
-     email: "jjohnson@email.com",
-     purchases: ["Small Package", "XL Bundle"]
+     Email: "jjohnson@email.com",
+     Purchases: ["Small Package", "XL Bundle"]
     }, 
     {Name: "Kathy Lewis",
-    email: "kathyl123@email.com",
-    purchases: ["Medium Package", "Large Bundle", "XL Bundle"]
+    Email: "kathyl123@email.com",
+    Purchases: ["Medium Package", "Large Bundle", "XL Bundle"]
     }, 
     {Name: "Lilly Blue",
-        email: "lillybillyblue@email.com", 
-        purchases: ["Small Package", "Medium Package", "XL Bundle"]
+        Email: "lillybillyblue@email.com", 
+        Purchases: ["Small Package", "Medium Package", "XL Bundle"]
     }
 ];
+
+customers.push({
+    Name: "Amanda Shelby",
+    Email: "amandashelby@email.com",
+    Purchases: ["Larage Bundle, XL Bundle"]
+});
+
+customers.shift()
+
